0c15a1127ce27b4cf1ed2c1a25aef28da643d789
nickKearns <nickkearns133@gmail.com>
2020-04-22 19:19:42 -0400

    Users can browse products

--- ProductHuntAssignment/FeedViewController.swift
+++ ProductHuntAssignment/FeedViewController.swift
@@ -16,30 +16,50 @@ class FeedViewController: UIViewController {
     
     // Array of Post objects to simulate real data coming in
     // Make sure each property that we're assigning to a UI element has a value for each mock Post.
-    var mockData: [Post] = {
-        var meTube = Post(id: 0, name: "MeTube", tagline: "Stream videos for free!", votesCount: 25, commentsCount: 4)
-        var boogle = Post(id: 1, name: "Boogle", tagline: "Search anything!", votesCount: 1000, commentsCount: 50)
-        var meTunes = Post(id: 2, name: "meTunes", tagline: "Listen to any song!", votesCount: 25000, commentsCount: 590)
-        
-        return [meTube, boogle, meTunes]
-    }()
+//    var mockData: [Post] = {
+//        var meTube = Post(id: 0, name: "MeTube", tagline: "Stream videos for free!", votesCount: 25, commentsCount: 4)
+//        var boogle = Post(id: 1, name: "Boogle", tagline: "Search anything!", votesCount: 1000, commentsCount: 50)
+//        var meTunes = Post(id: 2, name: "meTunes", tagline: "Listen to any song!", votesCount: 25000, commentsCount: 590)
+//
+//        return [meTube, boogle, meTunes]
+//    }()
+//
+    
+    var posts: [Post] = [] {
+        didSet {
+            feedTableView.reloadData()
+        }
+    }
+    
+    
+    let networkManager = NetworkManager()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         feedTableView.delegate = self
         feedTableView.dataSource = self
+        updateFeed()
         // Do any additional setup after loading the view.
     }
     
     
+    func updateFeed() {
+        networkManager.getPosts() { result in
+            self.posts = result
+        }
+    }
+    
 }
+
+
+
 
 
 extension FeedViewController: UITableViewDataSource {
     /// Determines how many cells will be shown on the table view.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return mockData.count
+        return posts.count
     }
     
     /// Creates and configures each cell.
@@ -47,7 +67,7 @@ extension FeedViewController: UITableViewDataSource {
         // dequeue and return an available cell, instead of creating a new cell
         let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath) as! PostTableViewCell
         
-        let post = mockData[indexPath.row]
+        let post = posts[indexPath.row]
         
         cell.post = post
         
